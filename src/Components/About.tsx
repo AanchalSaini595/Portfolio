72cc2851c6b1a7ee6f0b3a10283c70e52b0e39a1
Aanchal Saini <anchalsaini595@gmail.com>
2026-07-05 16:27:13 +0530

    Fix image sizing with fixed dimensions

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,7 +5,7 @@ export default function About()
   <div className="container mt-5">
     <div className="row align-items-center">
       <div className="col-md-5 text-center mb-4">
-        <img src="/aanchal_v2_headshot.jpg"  className="img-fluid shadow rounded-circle" />
+        <img src="/aanchal_v2_headshot.jpg" alt="Aanchal Saini" className="shadow rounded-circle" style={{width: '160px', height: '160px', objectFit: 'cover'}}/>
       </div>
 
       <div className="col-md-7">
