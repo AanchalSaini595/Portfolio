4df3dffbf9a418ba84b89dd200685572ed7d095e
Aanchal Saini <anchalsaini595@gmail.com>
2026-07-05 16:16:01 +0530

    changed size of image

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,7 +5,7 @@ export default function About()
   <div className="container mt-5">
     <div className="row align-items-center">
       <div className="col-md-5 text-center mb-4">
-        <img src="my-app/public/aanchal_v2_headshot.jpg"  className="img-fluid shadow rounded-circle" style={{maxWidth: '220px', width: '220px', height: '220px', objectFit: 'cover'}}/>
+        <img src="my-app/public/aanchal_v2_headshot.jpg"  className="img-fluid shadow rounded-circle" />
       </div>
 
       <div className="col-md-7">
